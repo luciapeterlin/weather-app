9c7d62456d57c0893f3bf1a2da4ce7be2b1d53b7
luciapeterlin <80421327+luciapeterlin@users.noreply.github.com>
2023-02-20 19:54:25 -0300

    Update weatherData.js

--- src/services/weatherData.js
+++ src/services/weatherData.js
@@ -3,14 +3,6 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_API_BASE_URL
 const API_KEY = process.env.REACT_APP_API_KEY
 
-// const weatherData = async (query) =>{
-//   const URL = `${base_url}key=${api_key}&q=%${query}`;
-//   const req = await axios(URL)
-//   return req
-// }
-
-// export default weatherData;
-
 export const getWeatherData = (query) => {
-  return axios.get(`${BASE_URL}key=${API_KEY}&q=%${query}}`)
+  return axios.get(`${BASE_URL}key=${API_KEY}&q=%${query}`)
 }
